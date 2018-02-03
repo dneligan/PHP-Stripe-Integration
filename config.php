635576fdc1b5ae8eb99b7b5bd17187355b84b29b
diarmuidn4 <diarmuidn4@gmail.com>
2018-02-03 13:34:41 +0000

    changing formatting

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 require_once('vendor/autoload.php');
 
 $stripe = array(
-  "secret_key"      => "pk_test_2UZks67mel2s6DcisSwbPvhK",
-  "publishable_key" => "sk_test_Q8KtcRgBcB7fnzqkA4tVEcIS"
+  "secret_key"      => 'pk_test_2UZks67mel2s6DcisSwbPvhK',
+  "publishable_key" => 'sk_test_Q8KtcRgBcB7fnzqkA4tVEcIS'
 );
 
 \Stripe\Stripe::setApiKey($stripe['secret_key']);
